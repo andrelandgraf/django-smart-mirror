21c4044a2a267e66ae1234b232599302c9356bf4
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-02-19 16:00:08 +0100

    Hello World Commit

--- smartmirror/views.py
+++ smartmirror/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+
+def index(request):
+    datetime = str(timezone.now().date())+" "+str(timezone.now().time())
+    return HttpResponse("Hello World: "+datetime)
 
 # Create your views here.
