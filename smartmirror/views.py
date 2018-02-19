a3f9482ed58d929e8f2e4dd356eb3b0fe342f39d
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-02-19 19:24:12 +0100

    Img test implementation

--- smartmirror/views.py
+++ smartmirror/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from django.template import loader
+from django.contrib import sessions
+
 
 def index(request):
     datetime = str(timezone.now().date())+" "+str(timezone.now().time())
